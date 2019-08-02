ea21dec301728c5c2e947d9c5b8a6ed8dd34e4df
Jonathan Hall <flimzy@flimzy.com>
2019-08-02 13:10:21 +0200

    Switch from kivik.Status* to http.Status*

--- createdb_test.go
+++ createdb_test.go
@@ -1,6 +1,7 @@
 package fs
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/go-kivik/kivik"
@@ -13,7 +14,7 @@ func TestCreateDBUnauthorized(t *testing.T) {
 		t.Errorf("Expected error attempting to create FS database in '%s'\n", path)
 		return
 	}
-	if kivik.StatusCode(err) != kivik.StatusUnauthorized {
+	if kivik.StatusCode(err) != http.StatusUnauthorized {
 		t.Errorf("Expected Unauthorized error trying to create FS database in '%s', but got %s\n", path, err)
 	}
 }
